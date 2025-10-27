cbf64b7195bb4d213cf74d68c28ecccb9d99b744
Adia Hite <adiahite1@gmail.com>
2025-10-27 10:44:23 -0700

    Added say function

--- main.cpp
+++ main.cpp
@@ -12,10 +12,17 @@
 #include <iostream>
 using namespace std;
 
+void say(string s);
+
 int main()
 {
     cout << "Greeting.\n";
     cout << "Hello world!" << endl;
 
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
