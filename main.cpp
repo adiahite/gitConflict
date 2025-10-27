05ac9a93095551d2fe63b01989c55c36cbe27caf
Adia Hite <adiahite1@gmail.com>
2025-10-27 10:20:19 -0700

    Added main.cpp greeting.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author Adia Hite
+ * @brief 
+ * @version 0.1
+ * @date 2025-10-27
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Greeting.\n";
+
+    return 0;
+}
